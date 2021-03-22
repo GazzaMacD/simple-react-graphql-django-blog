9fbf50a541600d160d77aef1732505a4da1fddcb
Gareth Macdonald <macdonald.gareth@gmail.com>
2021-03-22 10:59:41 +0100

    added basic blog app with basci post and category model

--- backend/app/blog/models.py
+++ backend/app/blog/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(
+        _("Name"),
+        max_length=100,
+        help_text=_("If you delete a category you will delete all the blog posts associated with this category. TAKE CARE")
+    )
+
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+    title = models.CharField(
+        _("Title"),
+        max_length=100
+        )
+    content = models.TextField(
+        _("Content"),
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        related_name="posts", 
+    )
+
+    def __str__(self):
+        return self.name
