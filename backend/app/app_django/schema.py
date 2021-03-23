223cb94622870b342b58bd4fef46ce2ff40e55f5
Gareth Macdonald <macdonald.gareth@gmail.com>
2021-03-23 11:22:45 +0100

    change grahpql schema, add get by slug resolvers to both Category and Post models

--- backend/app/app_django/schema.py
+++ backend/app/app_django/schema.py
@@ -6,34 +6,45 @@ from blog.models import Category, Post
 class CategoryType(DjangoObjectType):
     class Meta:
         model = Category
-        fields = ("id", "name", "posts")
+        fields = ("uuid", "name", "slug", "posts")
 
 class PostType(DjangoObjectType):
     class Meta:
         model = Post 
-        fields = ("id", "title", "content", "category")
+        fields = ("uuid", "title", "slug", "content", "category")
 
 class Query(graphene.ObjectType):
     all_posts = graphene.List(
         PostType
     )
+    post_by_slug = graphene.Field(
+        PostType,
+        slug=graphene.String(required=True)
+    )
     all_categories = graphene.List(
         CategoryType
     )
-    category_by_name = graphene.Field(
+    category_by_slug = graphene.Field(
         CategoryType,
-        name=graphene.String(required=True)
+        slug=graphene.String(required=True)
     )
-
+    """ Post resolvers """
     def resolve_all_posts(root, info):
         return Post.objects.select_related("category").all()
 
+    def resolve_post_by_slug(root, info, slug):
+        try:
+            return Post.objects.get(slug=slug)
+        except Post.DoesNotExist:
+            return None
+
+    """ Category resolvers """
     def resolve_all_categories(root, info):
         return Category.objects.all()
 
-    def resolve_category_by_name(root, info, name):
+    def resolve_category_by_slug(root, info, slug):
         try:
-            return Category.objects.get(name=name)
+            return Category.objects.get(slug=slug)
         except Category.DoesNotExist:
             return None
 
